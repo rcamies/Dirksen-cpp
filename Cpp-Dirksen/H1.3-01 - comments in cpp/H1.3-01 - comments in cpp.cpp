ce01c88486b0454f51a9807b445b415d2ad0b470
rcamies <none@none>
2023-04-25 21:29:11 +0200

    first course program (H1.3-01)

---
+++ Cpp-Dirksen/H1.3-01 - comments in cpp/H1.3-01 - comments in cpp.cpp
@@ -0,0 +1,16 @@
+/************************************************************/
+/* P01-F02 Markeren van commentaren in C++ */
+/************************************************************/
+void main(void)
+{
+    //instructie        // Commentaren in C++ beginnen met het
+        //instructie        // teken '//'. Aan het einde van het
+        //instructie        / commentaar is geen markering nodig.
+        // Als het commentaar meerdere regels beslaat, dan moet elke
+        // volgende commentaar-regel opnieuw worden gemarkeerd met
+        // het teken '//'.
+        /* Commentaar mag ook worden gemarkeerd met de tekens die in
+        'C' gangbaar zijn. Dan behoeft niet elke commentaar-regel
+        te worden gemarkeerd. Vergeet dan niet het eindteken:
+        */
+}
